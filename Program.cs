57afebad294d6d7a37117f0170e11c41abd24438
Aleksander Papiez <44852466+Koouk@users.noreply.github.com>
2021-01-04 14:34:02 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GOclient
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
